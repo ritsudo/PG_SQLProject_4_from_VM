19ef5ed75eaf11e92ef4c1fa1075ba85cd7a8a88
ritsudo <minato-namikaje@yandex.ru>
2023-09-22 02:38:40 +0000

    feat: added found element prompt

--- element.sh
+++ element.sh
@@ -8,16 +8,26 @@ if [[ -z $1 ]]
     #default parameters
     ELEMENT_FOUND=0
 
-    ELEMENT_NUMBER=0
-    #check if $1 = digit else
-    #check is $1 = 2 letters (Li) else
-    #check if $1 = Word (Lithium)
+    RESULT_ELEMENT_NUMBER=0
+    RESULT_ELEMENT_NAME_FULL='Default'
+    RESULT_ELEMENT_NAME_SHORT='Df'
+    RESULT_ELEMENT_TYPE_ID=0
+    RESULT_ELEMENT_TYPE='default'
+    RESULT_ELEMENT_MASS=1.000
+    RESULT_ELEMENT_MELTING_POINT=0
+    RESULT_ELEMENT_BOILINT_POINT=0
+
+    #check if $1 = digit else, get ID
+    #check is $1 = 2 letters (Li), get ID else
+    #check if $1 = Word (Lithium), get ID
 
     #if element found
     if [[ $ELEMENT_FOUND != 0 ]]
     then
-      echo Element found
+      #update element details
+
       #show element details
+      echo "The element with atomic number $RESULT_ELEMENT_NUMBER is $RESULT_ELEMENT_NAME_FULL ($RESULT_ELEMENT_NAME_SHORT). It's a $RESULT_ELEMENT_TYPE, with a mass of $RESULT_ELEMENT_MASS amu. $RESULT_ELEMENT_NAME_FULL has a melting point of $RESULT_ELEMENT_MELTING_POINT celsius and a boiling point of $RESULT_ELEMENT_BOILINT_POINT celsius."
     else
       echo "I could not find that element in the database."
     fi
